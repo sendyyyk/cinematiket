cab3be76964ab37d5a6eda07157364ed9fee5887
sendyyyk <sendy.2k18@gmail.comAAAAABBBBBBBBBBBBBBBBBBBB>
2024-06-24 00:09:34 +0700

    332

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,13 +1,13 @@
     document.addEventListener("DOMContentLoaded", function () {
         // Ip Public User 
-        
+
         fetch('https://api.ipify.org?format=json')
         .then(response => response.json())
         .then(data => {
             document.getElementById("ip-set").textContent = ` ${data.ip}`;
         })
         .catch(error => {
-            document.getElementById("ip-set").textContent = `Anda sedang offline | ${error}`;
+            error;
         });
         
         //  Media Device Change [START]
@@ -28,6 +28,23 @@
         })
         // Hamburger Menu Button [END]
         
+        // Tombol Filter Kategori [START]
+
+        const genresSelect = document.getElementById("genres-select")
+        genresSelect.addEventListener("change", function () {
+            const selectedOption = genresSelect.options[genresSelect.selectedIndex];
+            const selectedGenre = selectedOption.getAttribute('data-genres');
+            if (selectedGenre === "sedang-tayang") {
+                document.querySelector(".text-filter").textContent = "Sedang Tayang";
+            } else if (selectedGenre === "segera-tayang") {
+                document.querySelector(".text-filter").textContent = "Segera Tayang";
+            } else {
+                document.querySelector(".text-filter").textContent = "Populer";
+            }
+        });
+
+        // Tombol Filter Kategori [END]
+
         // Form Login/Registrasi/Lupa Password [START]
 
         document.getElementById("login-btn").addEventListener("click", function() {
@@ -93,7 +110,6 @@
         // Form Login/Registrasi/Lupa Password [END]
 
         // Hide / Show Password Button [START]
-
         const buttonPassword = document.querySelectorAll(".hide-pswd");
         
         buttonPassword.forEach(element => {
@@ -105,55 +121,8 @@
                 
             })
         })
-
         // Hide / Show Password Button [END]
 
-        // Jumlah Film [Daftar Film]
-
-        const listFilm = document.querySelectorAll(".list-film");
-        const widthDaftarScroll = parseFloat(window.getComputedStyle(document.getElementById("display-scroll")).width);
-        const heightDaftarScroll = parseFloat(window.getComputedStyle(document.getElementById("display-scroll")).height);
-        const daftarFilm = document.getElementById("film-wrap");
-        const totalFilm = daftarFilm.childElementCount;
-        const widthlistFilm = daftarFilm.clientWidth / 5;
-        const heightlistFilm = daftarFilm.clientHeight / 1;
-        let maxFilm = 5;
-        let maxFilmMobile = 2;
-        let totalNewFilm = totalFilm - maxFilm;
-
-        if (document.body.classList.contains("mobile-view")) {
-            if (totalFilm > 2) {
-                let heightFilm = 0;
-                if (totalFilm % maxFilmMobile === 1) {
-                    heightFilm = (heightDaftarScroll + heightlistFilm) * (Math.floor(totalFilm / 2 / maxFilmMobile));
-                } else {
-                    heightFilm = (heightDaftarScroll + heightlistFilm) * (totalFilm / 2 / maxFilmMobile - 1);
-                }
-                daftarFilm.style.height = `${heightFilm}px`;
-                let index = 0;
-                let indexS = 0;
-                for (let i = 0; i <= totalFilm; i++) {
-                    if (i == 0) {
-                        listFilm[i].style.left = 0;
-                        listFilm[i].style.top = `${heightlistFilm * 0}px`;
-                    } else if (i % 2 == 1) {
-                        listFilm[i].style.right = 0;
-                        listFilm[i].style.top = `${heightlistFilm * index}px`;
-                        index++;
-                        indexS = index;
-                    } 
-                    else {
-                        listFilm[i].style.left = 0;
-                        listFilm[i].style.top = `${heightlistFilm * indexS}px`;
-                    }
-                }
-            }
-        } else {
-            if (totalFilm > 5) {
-                daftarFilm.style.width = `calc(${widthDaftarScroll}px + (${widthlistFilm}px * ${totalNewFilm}))`;
-            } 
-        }
-        
         // Shift Button [Daftar Film]
         const postPrevBtn = document.getElementById("poster-prev-btn");
         const postNextBtn = document.getElementById("poster-next-btn");
@@ -201,23 +170,94 @@
             });
         });
         
-        
-        // Tombol Filter Kategori [START]
+        // Pesan Button OK [Start] 
+        const popup = document.querySelector(".popup")
+        const popupOkBtn = document.querySelectorAll(".popup-ok-btn");
+        const popupCategory = document.querySelectorAll(".popup-category");
+        popupOkBtn.forEach(element => {
+            element.addEventListener("click", function () {
+                popupCategory.forEach(element2 => {
+                    if (!element2.classList.contains("hidden")) {
+                        element2.classList.add("hidden");
+                        popup.style.height = "0";
+                        console.log("xexe");
+                    }
+                })
+            })
+        })
+        // Pesan Button OK [END] 
 
-        console.log("berhasil");
-        const genresSelect = document.getElementById("genres-select")
-        genresSelect.addEventListener("change", function () {
-            const selectedOption = genresSelect.options[genresSelect.selectedIndex];
-            const selectedGenre = selectedOption.getAttribute('data-genres');
-            if (selectedGenre === "sedang-tayang") {
-                document.querySelector(".text-filter").textContent = "Sedang Tayang";
-                console.log("jinx kontol");
-            } else if (selectedGenre === "segera-tayang") {
-                document.querySelector(".text-filter").textContent = "Segera Tayang";
+        // Pesan Button [Start] 
+        const pesan = document.getElementById("pesan-btn");
+        pesan.addEventListener("click", function () {
+            const popupEvent = document.querySelector(".popup-info");
+            const popupText = document.querySelector(".popup-info .popup-span");
+            console.log(popupText);
+            if(0) {
+                // Jika Sudah Login
             } else {
-                document.querySelector(".text-filter").textContent = "Populer";
+                popup.style.height = "100%";
+                popupEvent.classList.remove("hidden");
+                popupText.textContent = "Anda Belum Login, Silahkan Login Terlebih Dahulu !";
             }
-        });
+        })        
+        // Pesan Button [END]
 
-        // Tombol Filter Kategori [END]
+        // Cart Button [Start] 
+        const cart = document.getElementById("cart-btn");
+        cart.addEventListener("click", function () {
+            const popupEvent = document.querySelector(".popup-cross");
+            const popupText = document.querySelector(".popup-cross .popup-span");
+            if(0) {
+                // Jika Sudah Login
+            } else {
+                popup.style.height = "100%";
+                popupEvent.classList.remove("hidden");
+                popupText.textContent = "Anda Belum Login, Silahkan Login Terlebih Dahulu !";
+            }
+        })        
+        // Pesan Button [END] 
+        
+        // Jumlah Film [Daftar Film]
+        const listFilm = document.querySelectorAll(".list-film");
+        const widthDaftarScroll = parseFloat(window.getComputedStyle(document.getElementById("display-scroll")).width);
+        const heightDaftarScroll = parseFloat(window.getComputedStyle(document.getElementById("display-scroll")).height);
+        const daftarFilm = document.getElementById("film-wrap");
+        const totalFilm = daftarFilm.childElementCount;
+        const widthlistFilm = daftarFilm.clientWidth / 5;
+        const heightlistFilm = daftarFilm.clientHeight / 1;
+        let maxFilm = 5;
+        let maxFilmMobile = 2;
+        let totalNewFilm = totalFilm - maxFilm;
+
+        if (document.body.classList.contains("mobile-view")) {
+            let heightFilm = 0;
+            if (totalFilm % maxFilmMobile === 1) {
+                heightFilm = (heightDaftarScroll + heightlistFilm) * (Math.floor(totalFilm / 2 / maxFilmMobile));
+            } else {
+                heightFilm = (heightDaftarScroll + heightlistFilm) * (totalFilm / 2 / maxFilmMobile - 1);
+            }
+            daftarFilm.style.height = `${heightFilm}px`;
+            let index = 0;
+            let indexS = 0;
+            for (let i = 0; i <= totalFilm; i++) {
+                if (i == 0) {
+                    listFilm[i].style.left = 0;
+                    listFilm[i].style.top = `${heightlistFilm * 0}px`;
+                } else if (i % 2 == 1) {
+                    listFilm[i].style.right = 0;
+                    listFilm[i].style.top = `${heightlistFilm * index}px`;
+                    index++;
+                    indexS = index;
+                } 
+                else {
+                    listFilm[i].style.left = 0;
+                    listFilm[i].style.top = `${heightlistFilm * indexS}px`;
+                }
+            }
+        } else {
+            if (totalFilm > 5) {
+                daftarFilm.style.width = `calc(${widthDaftarScroll}px + (${widthlistFilm}px * ${totalNewFilm}))`;
+            } 
+        }
     })
